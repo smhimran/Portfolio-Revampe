235532605f3ba58b5b5ea71efeb3b23bcd9b1a5d
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2021-02-07 13:18:23 +0600

    add: Projects section

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll > 300 && scroll <= 920) {
+    if (scroll > 300) {
       $("#nav-wrap").css("background", "rgba(68, 68, 68, 0.9)");
+    }
+    if (scroll > 300 && scroll <= 920) {
       $("#about-link").addClass("current");
       $("#home-link").removeClass("current");
       $("#resume-link").removeClass("current");
     }
-    if (scroll > 920) {
-      console.log("To resume");
+    if (scroll > 920 && scroll <= 3300) {
+      $("#home-link").removeClass("current");
+      $("#projects-link").removeClass("current");
       $("#about-link").removeClass("current");
       $("#resume-link").addClass("current");
     }
+
+    if (scroll > 3300) {
+      $("#about-link").removeClass("current");
+      $("#resume-link").removeClass("current");
+      $("#home-link").removeClass("current");
+      $("#projects-link").addClass("current");
+    }
+
     if (scroll <= 300) {
       $("#nav-wrap").css("background", "transparent");
       $("#about-link").removeClass("current");
